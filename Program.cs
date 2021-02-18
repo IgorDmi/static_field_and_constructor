531b99aaef9f0b98d06f8d0ee860ee7420df65f9
IgorDmi <60873886+IgorDmi@users.noreply.github.com>
2021-02-18 20:41:44 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace static_field_and_constructor
+{
+
+    class Test
+    {
+        int _testField;
+        static int sumCont;
+
+        public Test(int value)
+        {
+            _testField = value;
+        }
+
+        public void sumMethod()
+        {
+            Console.WriteLine(sumCont = sumCont  + _testField);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t1 = new Test(5);
+            Test t2 = new Test(5);
+
+            t1.sumMethod();
+            t2.sumMethod();
+        }
+    }
+}
